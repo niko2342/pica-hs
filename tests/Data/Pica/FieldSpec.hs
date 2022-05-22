e4b15b7a6bfb08b7da6233ce3c7f5e74774dcf73
Nico Wagner <nwagner84@protonmail.com>
2022-05-22 13:39:44 +0200

    Add `range` function

--- tests/Data/Pica/FieldSpec.hs
+++ tests/Data/Pica/FieldSpec.hs
@@ -10,11 +10,20 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "level" $ do
-    it "throws an exception if used with an invalid tag" $ do
-      evaluate (level (Field "345A" Nothing [])) `shouldThrow` anyException
-      evaluate (level (Field "!45A" Nothing [])) `shouldThrow` anyException
-
     it "returns the level of a field" $ do
       level (Field "003@" Nothing []) `shouldBe` Main
       level (Field "123A" Nothing []) `shouldBe` Local
       level (Field "234A" Nothing []) `shouldBe` Copy
+
+    it "throws an exception if used with an invalid tag" $ do
+      evaluate (level (Field "345A" Nothing [])) `shouldThrow` anyException
+      evaluate (level (Field "!45A" Nothing [])) `shouldThrow` anyException
+
+  describe "range" $ do
+    it "returns the range of a field" $ do
+      range (Field "003@" Nothing []) `shouldBe` "03"
+      range (Field "123A" Nothing []) `shouldBe` "23"
+      range (Field "234A" Nothing []) `shouldBe` "34"
+
+    it "throws an exception if used with an invalid tag" $ do
+      evaluate (range (Field "35A" Nothing [])) `shouldThrow` anyException
